e63d3d6992a63dc31dc158862bab59ca7b9f2e40
junhp9355 <junhp9355@naver.com>
2022-07-25 14:22:57 +0900

    할 일 삭제

---
+++ test.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mysql from "mysql2/promise";
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "sbsst",
+  password: "sbs123414",
+  database: "a9",
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+app.get("/todos", async (req, res) => {
+  const [rows] = await pool.query("SELCET * FROM todo ORDER BY id DESC");
+  res.json(rows);
+});
+
+app.get("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const [rows] = await pool.query(
+    `
+        SELECT *
+        FROM todo
+        WHERE id = ?
+        `,
+    [id]
+  );
+  if (rows.length === 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+  res.json(rows[0]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
